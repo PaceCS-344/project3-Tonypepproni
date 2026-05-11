313b9be3de5d04f8ce379f4cea6a45ad6ebaa050
Nic0ro <willarossi1@gmail.com>
2026-05-11 12:12:48 -0400

    meow

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/project3-Tonypepproni/',
+  base: '/Nicole-Rossi/',
   plugins: [
     react(),
     tailwindcss(),
